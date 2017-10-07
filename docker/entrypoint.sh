c5ea87521807d1bf86af5e0415cf56e9f16ab728
Przemysław Grzeszczak <przemyslaw.grzeszczak@gmail.com>
2017-10-07 19:39:19 +0000

    Add ability to commit and push from docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,6 +6,8 @@ echo "Starting with UID : $USER_ID"
 useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
 
 echo "alias ll=\"ls -al\"" >> /home/user/.bashrc
+su - user -c "git config --global push.default simple"
+ln -s /.host_ssh /home/user/.ssh
 
 export HOME=/home/user
 
