bf614f90ad926f72195c6f9ee264b3eeffba5f40
Przemysław Grzeszczak <przemyslaw.grzeszczak@gmail.com>
2017-10-08 02:29:53 +0200

    Add chunks for vendors and runtime

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,23 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const { CheckerPlugin } = require('awesome-typescript-loader');
+const {
+  CheckerPlugin
+} = require('awesome-typescript-loader');
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    main: './src/index.tsx'
+    main: './src/index.tsx',
+    vendor: [
+      'react',
+      'react-dom'
+    ]
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx']
   },
   module: {
-    loaders: [
-      {
-        test: /\.tsx?$/,
-        loader: 'awesome-typescript-loader',
-        include: path.resolve(__dirname, "src"),
-      }
-    ]
+    loaders: [{
+      test: /\.tsx?$/,
+      loader: 'awesome-typescript-loader',
+      include: path.resolve(__dirname, "src"),
+    }]
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
@@ -25,6 +30,12 @@ module.exports = {
       title: 'Docker Registry UI',
       template: path.resolve(__dirname, "src/index.html"),
       inject: true
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'runtime'
     }),
     new CheckerPlugin()
   ],
