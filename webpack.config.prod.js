f6c54bb5d39ae66b855ed3d90ea726908ac920c9
Przemysław Grzeszczak <przemyslaw.grzeszczak@gmail.com>
2017-10-08 14:21:25 +0200

    Build dist with prod mode

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,10 +1,16 @@
 const merge = require('webpack-merge');
+const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.config.common.js');
 
 module.exports = merge(common, {
   plugins: [
-    new UglifyJSPlugin()
+    new UglifyJSPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
   ],
   output: {
     filename: '[name].[chunkhash].bundle.js'
