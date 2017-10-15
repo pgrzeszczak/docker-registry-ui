4c64ab2662cfd4180be7cf47036a05f2d3f666dc
Przemysław Grzeszczak <przemyslaw.grzeszczak@gmail.com>
2017-10-15 18:24:33 +0200

    Enable sudo in container

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,6 +4,7 @@ USER_ID=${LOCAL_USER_ID:-9001}
 
 echo "Starting with UID : $USER_ID"
 useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
+echo "user ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 echo "alias ll=\"ls -al\"" >> /home/user/.bashrc
 su - user -c "git config --global push.default simple"
